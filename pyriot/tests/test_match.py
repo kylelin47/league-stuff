a97e7445ff42fc3331f7cf9afa96fea29267b037
Kyle Lin <kylelin47@gmail.com>
2015-08-12 11:00:18 -0400

    Rename match history test

--- pyriot/tests/test_match.py
+++ pyriot/tests/test_match.py
@@ -8,10 +8,11 @@ api_key = os.environ['RIOT_API_KEY']
 class TestMatchHistoryRetriever(unittest.TestCase):
     def setUp(self):
         self.mha = MatchHistoryAccessor(api_key, 'na')
+        self.player_name = 'efdf'
 
-    def test_data_is_retrieved(self):
-        self.assertIsNotNone(self.mha.get_match_history('efdf')['matches'])
-
+    def test_player_matches_retrieved(self):
+        self.assertIsNotNone(
+            self.mha.get_match_history(self.player_name)['matches'])
 
 if __name__ == '__main__':
     unittest.main()
