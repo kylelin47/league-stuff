f0d7149c1a35121c9a56bd0202e00f46f927523b
Kyle Lin <kylelin47@gmail.com>
2015-08-11 23:11:48 -0400

    Add api_key property to ApiAccessor

--- match.py
+++ match.py
@@ -6,7 +6,15 @@ class ApiAccessor:
 
     def __init__(self, api_key):
         self.api_key = api_key
-        self.payload = {'api_key' : api_key}
+
+    @property
+    def api_key(self):
+        return self._api_key
+
+    @api_key.setter
+    def api_key(self, api_key):
+        self._api_key = api_key
+        self._payload = {'api_key' : api_key}
 
     def get_id(self, player_name):
         request_url = ('https://na.api.pvp.net/api/lol/na/v1.4/summoner'
@@ -14,7 +22,7 @@ class ApiAccessor:
         return self.get_json(request_url)[player_name]['id']
 
     def get_json(self, url):
-        return requests.get(url, params=self.payload).json()
+        return requests.get(url, params=self._payload).json()
 
 class MatchHistoryRetriever(ApiAccessor):
 
@@ -22,8 +30,6 @@ class MatchHistoryRetriever(ApiAccessor):
         return self.get_json(
             'https://na.api.pvp.net/api/lol/na/v2.2/matchhistory/{}'.format(
             self.get_id(player_name)))
-
-
 
 if __name__ == '__main__':
     api_key = os.environ['RIOT_API_KEY']
