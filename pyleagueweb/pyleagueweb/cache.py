c8e8c6ed76437dab6595ed16797603749ee36b8b
Kyle Lin <kylelin47@gmail.com>
2015-08-16 21:00:43 -0400

    Refactoring

--- pyleagueweb/pyleagueweb/cache.py
+++ pyleagueweb/pyleagueweb/cache.py
@@ -1,8 +1,61 @@
+import logging
+import os
+
 from werkzeug.contrib.cache import SimpleCache
+
+import pyleague.accessor as accessor
+import pyleague.exceptions
+import pyleague.match as match
+
+logger = logging.getLogger(__name__)
 
 id_cache = SimpleCache(default_timeout=60 * 60 * 24 * 7)
 history_cache = SimpleCache()
 null_value = 'N/A'
 
+api_key = os.environ['RIOT_API_KEY']
+
+
 def generate_cache_key(region, name):
     return region.upper() + ''.join(name.lower().split())
+
+
+def get_id(region, name):
+    cache_key = generate_cache_key(region, name)
+    player_id = id_cache.get(cache_key)
+    if player_id == null_value:
+        return None
+    if player_id is None:
+        api = accessor.ApiAccessor(api_key, region)
+        try:
+            player_id = api.get_id(name)
+        except pyleague.exceptions.PyLeagueError as e:
+            logger.warning(e)
+            if '404' in str(e):
+                player_id = null_value
+            return None
+        finally:
+            id_cache.set(cache_key, player_id)
+    return player_id
+
+
+def get_history(region, name):
+    cache_key = generate_cache_key(region, name)
+    history = history_cache.get(cache_key)
+    if history == null_value:
+        return None
+    if history is None:
+        mha = match.MatchHistoryAccessor(api_key, region)
+        player_id = get_id(region, name)
+        try:
+            history = mha.get_match_history(player_id=player_id)
+        except TypeError:
+            return None
+        except pyleague.exceptions.PyLeagueError as e:
+            logger.warning(e)
+            if '404' in str(e):
+                history = null_value
+            return None
+        finally:
+            history_cache.set(cache_key, history)
+    return history
--- pyleagueweb/pyleagueweb/player/player.py
+++ pyleagueweb/pyleagueweb/player/player.py
@@ -1,17 +1,10 @@
-import logging
-import os
-
 from flask import Blueprint
 from flask import render_template
 
-import pyleague.exceptions
 import pyleague.match as match
 import pyleagueweb.cache as cache
 
-logger = logging.getLogger(__name__)
 player_api = Blueprint('player_api', __name__)
-
-api_key = os.environ['RIOT_API_KEY']
 
 
 class Player:
@@ -23,7 +16,7 @@ class Player:
 
 @player_api.route('/<region>/<name>/')
 def show_player(region, name):
-    history = get_match_history(region, name)
+    history = cache.get_history(region, name)
     if history is None:
         return render_template('errors/other.html',
                                error='data unavailable. make sure this guy '
@@ -36,31 +29,4 @@ def show_player(region, name):
                matches=match.get_number_of_matches(history),
                contributions=match.get_first_blood_contributions(history)
                )
-    return render_template('player.html',
-                           player=p)
-
-
-def get_match_history(region, name):
-    cache_key = cache.generate_cache_key(region, name)
-    history = cache.history_cache.get(cache_key)
-    if history == cache.null_value:
-        return None
-    if history is None:
-        mha = match.MatchHistoryAccessor(api_key, region)
-        player_id = cache.id_cache.get(cache_key)
-        if player_id == cache.null_value:
-            return None
-        try:
-            if player_id is None:
-                player_id = mha.get_id(name)
-            history = mha.get_match_history(player_id=player_id)
-        except pyleague.exceptions.PyLeagueError as e:
-            logger.warning(e)
-            if '404' in str(e):
-                player_id = cache.null_value
-                history = cache.null_value
-            return None
-        finally:
-            cache.id_cache.set(cache_key, player_id)
-            cache.history_cache.set(cache_key, history)
-    return history
+    return render_template('player.html', player=p)
